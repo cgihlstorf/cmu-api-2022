ec683692432c8b2dc8d3a95f46a6f3e8d36f62a8
Caroline Gihlstorf <cgihlstorf@haverford.edu>
2022-06-02 14:45:23 -0400

    change error message construct, edited AST output

--- extract_func_calls.py
+++ extract_func_calls.py
@@ -3,6 +3,7 @@ import ast
 
 #TODO make comments nicer
 #TODO still need to take into account variables (create a symbol table?)
+#TODO torch regex isn't working in the first function
 
 '''Takes a Python file and a two-letter code ('tf' (for Tensorflow) or 'pt' (For PyTorch)). 
 Returns a list of all of the (either Tensorflow or PyTorch) function calls found in the code.'''
@@ -30,35 +31,32 @@ def regex_extractor(python_file: str, api_name: str) -> list: #api_name is eithe
     return list_of_funcs
  
 #print(regex_extractor("test_tf.py", "tf"))
-print(regex_extractor("test_pt.py", "pt"))
+#print(regex_extractor("test_pt.py", "pt"))
 
 def AST_extractor(python_file: str, api_name: str) -> list: #as in the above function, api_name is either tf (Tensorflow) or pt (PyTorch)
     
     retrieved_ast = open(python_file, "r")
     python_tree = ast.parse(retrieved_ast.read())
     tree_string = ast.dump(python_tree, annotate_fields=False)
-    tree_list = tree_string.split("Assign") #not sure what to do here...
+    tree_list = re.split("\([A-Z]", tree_string) #not sure what to do here...
     list_of_funcs = []
 
     #print(tree_string)
-    for item in tree_list:
-        print(item)
+    #how to represent and work with the AST???
 
-    if api_name == "tf":
-        #specify for tf 
-        x = 1 #placeholder for now
-    elif api_name == "pt":
-        #specify for pt
-        x = 2 #placeholder for now
-    else:
+    if (api_name != "tf" and api_name != "pt"):
         raise ValueError("Invalid entry to function AST_extractor(). Must be either 'tf' (Tensorflow) or 'pt' (PyTorch)") 
 
-    return list_of_funcs
+    for item in tree_list:
+        if api_name in item:
+            item = item.replace(item[0:3], "", -1)
+            item_as_list = item.split(",")
+            list_of_funcs.append(item_as_list[0:len(item_as_list) - 3])
+
+    return list_of_funcs[0]
   
 
-
-
-#print(AST_extractor("test_tf.py", "tf"))
+print(AST_extractor("test_tf.py", "tf"))
 #print(AST_extractor("test_pt.py", "pt"))
     
 
