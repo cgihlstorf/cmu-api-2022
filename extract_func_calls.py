6a32667ea33879177c3f17ae4244196830d8b6e2
Caroline Gihlstorf <cgihlstorf@haverford.edu>
2022-06-02 14:00:38 -0400

    Update documentation; add raising errors

--- extract_func_calls.py
+++ extract_func_calls.py
@@ -7,7 +7,7 @@ import ast
 '''Takes a Python file and a two-letter code ('tf' (for Tensorflow) or 'pt' (For PyTorch)). 
 Returns a list of all of the (either Tensorflow or PyTorch) function calls found in the code.'''
 def regex_extractor(python_file: str, api_name: str) -> list: #api_name is either 'tf' (for Tensorflow) or 'pt' (For PyTorch)
-    return_list = []
+    list_of_funcs = []
 
     file = open(python_file, "r")
     file_lines = file.readlines()
@@ -19,32 +19,46 @@ def regex_extractor(python_file: str, api_name: str) -> list: #api_name is eithe
     elif api_name == "pt":
         regex = "torch\.+.*" #I'm trying to do "torch\.+^org" but it isn't working...
     else:
-        print("Invalid entry to function regex_extractor(). Must be either 'tf' (for Tensorflow) or 'pt' (For PyTorch)") 
+        raise ValueError("Invalid entry to function regex_extractor(). Must be either 'tf' (Tensorflow) or 'pt' (PyTorch)") 
 
     #Compile a list of all relevant function calls
     for line in file_lines:
         line_list = re.findall(regex, line)
         if line_list != []: 
-            return_list.append(line_list[0])
+            list_of_funcs.append(line_list[0])
     
-    return return_list
+    return list_of_funcs
  
 #print(regex_extractor("test_tf.py", "tf"))
-#print(regex_extractor("test_pt.py", "pt"))
+print(regex_extractor("test_pt.py", "pt"))
 
-def AST_extractor(python_file: str, api_name: str):
+def AST_extractor(python_file: str, api_name: str) -> list: #as in the above function, api_name is either tf (Tensorflow) or pt (PyTorch)
+    
     retrieved_ast = open(python_file, "r")
     python_tree = ast.parse(retrieved_ast.read())
     tree_string = ast.dump(python_tree, annotate_fields=False)
     tree_list = tree_string.split("Assign") #not sure what to do here...
+    list_of_funcs = []
+
     #print(tree_string)
     for item in tree_list:
         print(item)
-    return []
+
+    if api_name == "tf":
+        #specify for tf 
+        x = 1 #placeholder for now
+    elif api_name == "pt":
+        #specify for pt
+        x = 2 #placeholder for now
+    else:
+        raise ValueError("Invalid entry to function AST_extractor(). Must be either 'tf' (Tensorflow) or 'pt' (PyTorch)") 
+
+    return list_of_funcs
+  
 
 
 
-print(AST_extractor("test_tf.py", "tf"))
+#print(AST_extractor("test_tf.py", "tf"))
 #print(AST_extractor("test_pt.py", "pt"))
     
 
