5399637c778f94a2286d2602ff4a3e067f5719e0
Caroline Gihlstorf <cgihlstorf@haverford.edu>
2022-06-02 11:04:31 -0400

    committing file of function extraction methods

---
+++ extract_func_calls.py
@@ -0,0 +1,53 @@
+import re
+import ast
+
+#TODO make comments nicer
+#TODO still need to take into account variables (create a symbol table?)
+
+'''Takes a Python file and a two-letter code ('tf' (for Tensorflow) or 'pt' (For PyTorch)). 
+Returns a list of all of the (either Tensorflow or PyTorch) function calls found in the code.'''
+def regex_extractor(python_file: str, api_name: str) -> list: #api_name is either 'tf' (for Tensorflow) or 'pt' (For PyTorch)
+    return_list = []
+
+    file = open(python_file, "r")
+    file_lines = file.readlines()
+    regex = ""
+
+    #Determine whether to seach for Tensorflow or PyTorch functions
+    if api_name == "tf":
+        regex = "(tf\.+.*|tensorflow\.+^org)" 
+    elif api_name == "pt":
+        regex = "torch\.+.*" #I'm trying to do "torch\.+^org" but it isn't working...
+    else:
+        print("Invalid entry to function regex_extractor(). Must be either 'tf' (for Tensorflow) or 'pt' (For PyTorch)") 
+
+    #Compile a list of all relevant function calls
+    for line in file_lines:
+        line_list = re.findall(regex, line)
+        if line_list != []: 
+            return_list.append(line_list[0])
+    
+    return return_list
+ 
+#print(regex_extractor("test_tf.py", "tf"))
+#print(regex_extractor("test_pt.py", "pt"))
+
+def AST_extractor(python_file: str, api_name: str):
+    retrieved_ast = open(python_file, "r")
+    python_tree = ast.parse(retrieved_ast.read())
+    tree_string = ast.dump(python_tree, annotate_fields=False)
+    tree_list = tree_string.split("Assign") #not sure what to do here...
+    #print(tree_string)
+    for item in tree_list:
+        print(item)
+    return []
+
+
+
+print(AST_extractor("test_tf.py", "tf"))
+#print(AST_extractor("test_pt.py", "pt"))
+    
+
+
+
+
