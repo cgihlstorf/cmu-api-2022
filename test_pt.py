18b98afebbf92b09664c931db52434ae1aaed917
Caroline Gihlstorf <cgihlstorf@haverford.edu>
2022-06-02 11:06:34 -0400

    committing test files for extraction functions

---
+++ test_pt.py
@@ -0,0 +1,9 @@
+#from https://pytorch.org/docs/stable/tensors.html
+
+torch.zeros([2, 4], dtype=torch.int32)
+tensor([[ 0,  0,  0,  0],
+        [ 0,  0,  0,  0]], dtype=torch.int32)
+cuda0 = torch.device('cuda:0')
+torch.ones([2, 4], dtype=torch.float64, device=cuda0)
+tensor([[ 1.0000,  1.0000,  1.0000,  1.0000],
+        [ 1.0000,  1.0000,  1.0000,  1.0000]], dtype=torch.float64, device='cuda:0')
